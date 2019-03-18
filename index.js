db8210043482c9ad0502bd6d62bf6c2cbef3898a
Michael Bey <michael.bey@stu.bmcc.cuny.edu>
2019-03-18 23:40:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 
-Function destructivelyAppendDriver(drivers){
-  drivers.push("ralph")
+Function destructivelyAppendDriver(drivers.push()){
+  return drivers
 }
 
